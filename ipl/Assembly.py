edc092d71755dd6944574ff271f9e88b31061bcb
Varshith <varshith.lfhs@gmail.com>
2018-04-13 21:02:11 +0530

    add comments, global data, directives, table_size; todo: control_blocks, statements

--- ipl/Assembly.py
+++ ipl/Assembly.py
@@ -1,5 +1,15 @@
 import pprint
 
+dot_text_string = "The .text assembler directive indicates"
+global_func_string = "The following is the code"
+
+save_ra_string = "Save the return address"
+save_fp_string = "Save the frame pointer"
+update_fp_string = "Update the frame pointer"
+space_locals_string = "Make space for the locals"
+
+return_string = "Jump back to the called procedure"
+
 class Assembly:
 
     def initialise(self, symbol_table, ast):
@@ -8,11 +18,24 @@ class Assembly:
 
         self.code = []
 
-    def add_stat(self, string):
+    def add_stat(self, string, comment=None, indent=True):
+        string = str(string)
+        if string == "":
+            indent = False
+        if indent:
+            string = "\t" + string
+        if comment is not None:
+            string += "\t# %s" % comment
         self.code.append(string)
 
+    def add_raw_string(self, string):
+        self.add_stat(string, None, False)
+
     def size_table(self, func_name):
-        return 0
+        param_size = sum( [ self.get_type_size(type_dict) for name, type_dict in self.symbol_table.table[func_name]["parameters"].items() ] )
+        locals_size = sum( [ self.get_type_size(type_dict) for name, type_dict in self.symbol_table.table[func_name]["symbol_table"].items() ] )
+
+        return param_size + locals_size
 
     def gen_assembly_stat(self, statement):
 
@@ -20,27 +43,40 @@ class Assembly:
 
         pass
 
+    def gen_func_prologue_code(self, func_name, locals_space):
+
+        self.add_raw_string("# Prologue begins")
+        self.add_stat("sw $ra, 0($sp)", save_ra_string)
+        self.add_stat("sw $fp, -4($sp)", save_fp_string)
+        self.add_stat("sub $fp, $sp, 8", update_fp_string)
+        self.add_stat("sub $sp, $sp, %d" % ( locals_space ), space_locals_string)
+        self.add_raw_string("# Prologue begins")
+
     def gen_func_code(self, blockid):
 
-        self.free_registers = list(range(8))
-        func_name = self.ast.functions[blockid]
+        self.free_registers = \
+                ["s%d" % (x) for x in range(8)] + \
+                ["t%d" % (x) for x in range(8)]
+        self.free_registers.reverse()
 
+        func_name = self.ast.functions[blockid]
         locals_space = self.size_table(func_name)
 
-        # generate prologue
-        self.add_stat("%s:" % (func_name))
+        # directives
+        self.add_stat(".text", dot_text_string)
+        self.add_stat(".globl %s" % func_name, global_func_string)
 
-        self.add_stat("sw $ra, 0($sp)")
-        self.add_stat("sw $fp, -4($sp)")
-        self.add_stat("sub $fp, $sp, 8")
-        self.add_stat("sub $sp, $sp, %d" % ( locals_space ))
+        self.add_raw_string("%s:" % (func_name))
+
+        # generate prologue
+        self.gen_func_prologue_code(func_name, locals_space)
 
         # generate code for each block
         block = self.ast.blocks[blockid]
 
         while block.return_type == "false":
 
-            self.add_stat("label%d:" % (blockid))
+            self.add_raw_string("label%d:" % (blockid))
 
             if block.control_type:
 
@@ -64,24 +100,30 @@ class Assembly:
             blockid += 1
             block = self.ast.blocks[blockid]
 
-        self.add_stat("label%d:" % (blockid))
+        self.add_raw_string("label%d:" % (blockid))
         self.add_stat("j epilogue_%s" % (func_name))
 
         # generate epilogue
+        self.gen_func_epilogue_code(func_name, locals_space)
+
+        self.free_registers = []
+
+    def gen_func_epilogue_code(self, func_name, locals_space):
+
+        self.add_stat("")
+        self.add_raw_string("# Epilogue begins")
         self.add_stat("epilogue_%s:" % (func_name))
 
         self.add_stat("add $sp, $sp, %d" % (locals_space))
         self.add_stat("lw $fp, -4($sp)")
         self.add_stat("sw $ra, 0($sp)")
-        self.add_stat("jr $ra")
-
-        self.free_registers = []
-
-        pass
+        self.add_stat("jr $ra", return_string)
+        self.add_raw_string("# Epilogue ends")
 
     def generate_code(self):
 
         # generate code for global data and set headers
+        self.gen_global_var_code()
 
         # process each function
 
@@ -90,6 +132,34 @@ class Assembly:
 
 
         pass
+
+    def get_type_size(self, type_dict):
+        val = 4
+        if type_dict["base_type"] == "float" and type_dict["level"] == 0:
+            val = 8
+        return val
+
+    def get_directive_and_val(self, type_dict):
+        size = self.get_type_size(type_dict)
+        val = 0
+        typ = "word"
+        if size == 8:
+            typ = "space"
+            val = 8
+        return "." + typ, val
+
+    def gen_global_var_code(self):
+
+        self.add_stat("")
+        self.add_stat(".data")
+
+        for name, data in self.symbol_table.table.items():
+            if data["type"] == "procedure":
+                continue
+            typ, val = self.get_directive_and_val(data)
+            self.add_raw_string("global_%s: \t%s\t%d" % (name, typ, val))
+
+        self.add_stat("")
 
     def print_code(self):
         pp = pprint.PrettyPrinter(indent=4)
@@ -103,10 +173,9 @@ class Assembly:
         print("\nTemporaries:")
         pp.pprint(self.ast.temporaries)
 
-        print("\nCode:\n")
+        print("\nSymbol Table:")
+        pp.pprint(self.symbol_table.table)
+
+        print("\nCode:")
         for stat in self.code:
-            string = str(stat)
-            if string[-1] == ":":
-                print(string)
-            else:
-                print("\t", string)
+            print(stat)
